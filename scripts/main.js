bd92eec55bbce558f4e3d02ca3cf521061fe8df8
Johudo <dimazuev11@gmail.com>
2020-02-19 01:42:08 +0300

    commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// http://data.fixer.io/api/latest
+//     ? access_key = API_KEY
+//     & base = USD
+//     & symbols = GBP,JPY,EUR
+
+$(function() {
+	
+	$.get(
+		'http://data.fixer.io/api/latest',
+		
+		{
+			access_key: 'afbdceb14de20f5cf57afc7aa07f7ecb',
+			symbols: 'RUB,USD'
+		},
+
+		function (data) {
+			let eurRate = (data.rates.RUB).toFixed(6);
+			let usdRate = (data.rates.RUB / data.rates.USD).toFixed(6);
+
+			$('#USD-rate').text(usdRate + ' ₽');
+			$('#EUR-rate').text(eurRate + ' ₽');
+		}
+	);
+
+});
+
