81d4ca320ac43997d82fe3b0d46a133614554330
Johudo <dimazuev11@gmail.com>
2020-03-01 18:56:09 +0300

    Added Adaptivity

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,26 @@
 
 $(function() {
 	
+
+
+
+
+	$('.header__burger').click(function(event) {
+		event.preventDefault();
+
+		$('.nav-block').toggleClass('nav__actived');
+		$('.nav-element').toggleClass('nav-element__actived');
+		$('.header__burger').toggleClass('header__burger__actived');
+
+
+		if( $('.nav-block').hasClass('nav__actived') ) {
+			let navTop =  $('.black-bar').outerHeight() + $('.header__mobile-upper').outerHeight();
+			$('.nav__actived')[0].style.top = navTop + "px";
+		}
+		
+	});
+
+
 	$.get(
 		'http://data.fixer.io/api/latest',
 		
